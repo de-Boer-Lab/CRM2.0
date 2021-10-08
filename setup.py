380bd48c6fbd04056d5723fe2d7fe81fc1b03d83
Carl de Boer <Carldeboer@users.noreply.github.com>
2021-10-08 13:36:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from setuptools import setup
 from setuptools import find_packages
 setup(
-     name='CisRegModels',    # This is the name of your PyPI-package.
-     version='1.0',                          # Update the version number for new releases
+     name='CRM',    # This is the name of your PyPI-package.
+     version='2.0',                          # Update the version number for new releases
      description='Various scripts for processing GPRA data, as well as making models of cis-regulatory logic from GPRA data.',
      packages=find_packages(),
      url='https://github.com/Carldeboer/CisRegModels',
-     install_requires=['tensorflow','numpy','datetime'],
+     install_requires=['tensorflow==1.1.0','numpy','datetime'],
      scripts=['collapsePromoters.py','mergeSeqsByBowtie.py','seqsToOHC.py','translateSequencesByDict.py','alignFastqsIntoSeqs.py','makeThermodynamicEnhancosomeModel.py', 'predictThermodynamicEnhancosomeModel.py','startCRMServer.py','makeThermodynamicEnhancosomeModelFromCkpt.py','GASeqDesign.py']                  # The name of your scipt, and also the command you'll be using for calling it
 )
